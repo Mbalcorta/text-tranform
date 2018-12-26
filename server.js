a39e19320bd81aadfcc5bc050806818b3bdca9e0
Mateo Balcorta <misterrattail@gmail.com>
2018-12-25 21:07:08 -0800

    Add body parser and quote module.

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const quote = require('./quote.js');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('welcome!');
+  res.send(quote());
 });
 
 app.post('/sms', (req, res) => {
+  console.log('response from text ', req.body);
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message(req.body);
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
